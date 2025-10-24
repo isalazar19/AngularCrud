e8ef285fa4fe7eac00fae72280df9d1b14743524
Iván Salazar <ivan.condore@gmail.com>
2025-10-24 13:54:05 -0300

    carpeta para consultas (\Queries)

--- Queries/SQLQuery1.sql
+++ Queries/SQLQuery1.sql
@@ -1,31 +1,3 @@
-create database DBEmpleado
-
-use DBEmpleado
-
-create table Departamento(
-IdDepartamento int primary key identity,
-Nombre varchar(50),
-FechaCreacion datetime default getdate()
-)
-
-create table Empleado(
-IdEmpleado int primary key identity,
-NombreCompleto varchar(50),
-IdDepartamento int references Departamento(IdDepartamento),
-Sueldo int,
-FechaContrato datetime,
-FechaCreacion datetime default getdate()
-)
-
-insert into Departamento(Nombre) values
-('Administracion'),
-('Marketing'),
-('Ventas'),
-('Comercio')
-
-insert into Empleado(NombreCompleto,IdDepartamento,Sueldo,FechaContrato) values
-('Franco Hernandez',1,1400,getdate())
----------------------------------------------------------------------------------------
 select emp.IdEmpleado, emp.NombreCompleto, emp.IdDepartamento, dpto.Nombre, emp.Sueldo, emp.FechaContrato, emp.FechaCreacion
 from Empleado emp
 inner join Departamento dpto on dpto.IdDepartamento = emp.IdDepartamento
